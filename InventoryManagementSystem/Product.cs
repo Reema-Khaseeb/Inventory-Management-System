d5309573bd517959a2d45db72a3022752b2d80ab
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-07-26 16:01:17 +0300

    add properties for Product class

--- InventoryManagementSystem/Product.cs
+++ InventoryManagementSystem/Product.cs
@@ -8,6 +8,10 @@ namespace InventoryManagementSystem
 {
     public class Product
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
         
     }
 
