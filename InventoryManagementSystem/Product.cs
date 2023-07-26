c01df37c8a40a395b8d28fd70fd3a1791a8b3356
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-07-26 16:01:59 +0300

    add constructor for Product class

--- InventoryManagementSystem/Product.cs
+++ InventoryManagementSystem/Product.cs
@@ -7,12 +7,17 @@ using System.Threading.Tasks;
 namespace InventoryManagementSystem
 {
     public class Product
-    {
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
 
-        
+    public Product(string name, double price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
     }
+}
 
 }
